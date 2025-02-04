e1501538ba97e8baa94533240b03404bebd471ee
Roi <150252081+RoiCgovz@users.noreply.github.com>
2025-02-04 18:04:13 +0800

    Add files via upload

---
+++ Project 1/GroceryCalculationApp/Item.cs
@@ -0,0 +1,16 @@
+﻿namespace GroceryCalculationApp
+{
+    internal class Item
+    {
+        public double price;
+        public string name;
+        public int qty;
+        public Item(string name, int qty, double price)
+        {
+            this.name = name;
+            this.qty = qty;
+            this.price = price;
+        }
+
+    }
+}
