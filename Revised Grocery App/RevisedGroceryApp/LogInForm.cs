d688e06095d887c15000268212ab26e4e8e68f70
MakiAT12 <batter882@gmail.com>
2025-04-29 11:44:23 +0800

    LoginPages

--- Revised Grocery App/RevisedGroceryApp/LogInForm.cs
+++ Revised Grocery App/RevisedGroceryApp/LogInForm.cs
@@ -16,5 +16,10 @@ namespace RevisedGroceryApp
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
---
+++ Revised Grocery App/RevisedGroceryApp/SignForm.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace RevisedGroceryApp
+{
+    partial class LogInForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cuiTextBox22 = new CuoreUI.Controls.cuiTextBox2();
+            this.cuiTextBox21 = new CuoreUI.Controls.cuiTextBox2();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.cuiButton1 = new CuoreUI.Controls.cuiButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cuiButton2 = new CuoreUI.Controls.cuiButton();
+            this.cuiFormAnimator1 = new CuoreUI.Components.cuiFormAnimator(this.components);
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cuiTextBox22
+            // 
+            this.cuiTextBox22.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.cuiTextBox22.BackgroundColor = System.Drawing.Color.White;
+            this.cuiTextBox22.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.cuiTextBox22.BorderSize = 1;
+            this.cuiTextBox22.Content = "";
+            this.cuiTextBox22.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.cuiTextBox22.FocusBackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
+            this.cuiTextBox22.FocusBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(106)))), ((int)(((byte)(0)))));
+            this.cuiTextBox22.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cuiTextBox22.ForeColor = System.Drawing.Color.Gray;
+            this.cuiTextBox22.Location = new System.Drawing.Point(539, 344);
+            this.cuiTextBox22.Margin = new System.Windows.Forms.Padding(4);
+            this.cuiTextBox22.Multiline = false;
+            this.cuiTextBox22.Name = "cuiTextBox22";
+            this.cuiTextBox22.Padding = new System.Windows.Forms.Padding(15, 15, 15, 0);
+            this.cuiTextBox22.PasswordChar = false;
+            this.cuiTextBox22.PlaceholderColor = System.Drawing.SystemColors.WindowText;
+            this.cuiTextBox22.PlaceholderText = "";
+            this.cuiTextBox22.Rounding = new System.Windows.Forms.Padding(8);
+            this.cuiTextBox22.Size = new System.Drawing.Size(266, 45);
+            this.cuiTextBox22.TabIndex = 9;
+            this.cuiTextBox22.TextOffset = new System.Drawing.Size(0, 0);
+            this.cuiTextBox22.UnderlinedStyle = false;
+            // 
+            // cuiTextBox21
+            // 
+            this.cuiTextBox21.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.cuiTextBox21.BackgroundColor = System.Drawing.Color.White;
+            this.cuiTextBox21.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.cuiTextBox21.BorderSize = 1;
+            this.cuiTextBox21.Content = "";
+            this.cuiTextBox21.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.cuiTextBox21.FocusBackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
+            this.cuiTextBox21.FocusBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(106)))), ((int)(((byte)(0)))));
+            this.cuiTextBox21.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cuiTextBox21.ForeColor = System.Drawing.Color.Gray;
+            this.cuiTextBox21.Location = new System.Drawing.Point(539, 258);
+            this.cuiTextBox21.Margin = new System.Windows.Forms.Padding(4);
+            this.cuiTextBox21.Multiline = false;
+            this.cuiTextBox21.Name = "cuiTextBox21";
+            this.cuiTextBox21.Padding = new System.Windows.Forms.Padding(15, 15, 15, 0);
+            this.cuiTextBox21.PasswordChar = false;
+            this.cuiTextBox21.PlaceholderColor = System.Drawing.SystemColors.WindowText;
+            this.cuiTextBox21.PlaceholderText = "";
+            this.cuiTextBox21.Rounding = new System.Windows.Forms.Padding(8);
+            this.cuiTextBox21.Size = new System.Drawing.Size(266, 45);
+            this.cuiTextBox21.TabIndex = 8;
+            this.cuiTextBox21.TextOffset = new System.Drawing.Size(0, 0);
+            this.cuiTextBox21.UnderlinedStyle = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.panel1.Controls.Add(this.cuiButton1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(370, 601);
+            this.panel1.TabIndex = 7;
+            // 
+            // cuiButton1
+            // 
+            this.cuiButton1.CheckButton = false;
+            this.cuiButton1.Checked = false;
+            this.cuiButton1.CheckedBackground = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(106)))), ((int)(((byte)(0)))));
+            this.cuiButton1.CheckedForeColor = System.Drawing.Color.White;
+            this.cuiButton1.CheckedImageTint = System.Drawing.Color.White;
+            this.cuiButton1.CheckedOutline = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(106)))), ((int)(((byte)(0)))));
+            this.cuiButton1.Content = "Back to Login";
+            this.cuiButton1.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.cuiButton1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cuiButton1.ForeColor = System.Drawing.Color.White;
+            this.cuiButton1.HoverBackground = System.Drawing.Color.DarkGray;
+            this.cuiButton1.HoveredImageTint = System.Drawing.Color.White;
+            this.cuiButton1.HoverForeColor = System.Drawing.Color.White;
+            this.cuiButton1.HoverOutline = System.Drawing.Color.Empty;
+            this.cuiButton1.Image = null;
+            this.cuiButton1.ImageAutoCenter = true;
+            this.cuiButton1.ImageExpand = new System.Drawing.Point(0, 0);
+            this.cuiButton1.ImageOffset = new System.Drawing.Point(0, 0);
+            this.cuiButton1.Location = new System.Drawing.Point(113, 417);
+            this.cuiButton1.Name = "cuiButton1";
+            this.cuiButton1.NormalBackground = System.Drawing.Color.Black;
+            this.cuiButton1.NormalForeColor = System.Drawing.Color.White;
+            this.cuiButton1.NormalImageTint = System.Drawing.Color.White;
+            this.cuiButton1.NormalOutline = System.Drawing.Color.Empty;
+            this.cuiButton1.OutlineThickness = 1.6F;
+            this.cuiButton1.PressedBackground = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(106)))), ((int)(((byte)(0)))));
+            this.cuiButton1.PressedForeColor = System.Drawing.Color.White;
+            this.cuiButton1.PressedImageTint = System.Drawing.Color.White;
+            this.cuiButton1.PressedOutline = System.Drawing.Color.Empty;
+            this.cuiButton1.Rounding = new System.Windows.Forms.Padding(8);
+            this.cuiButton1.Size = new System.Drawing.Size(153, 45);
+            this.cuiButton1.TabIndex = 5;
+            this.cuiButton1.TextOffset = new System.Drawing.Point(0, 0);
+            this.cuiButton1.Click += new System.EventHandler(this.cuiButton1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.AliceBlue;
+            this.label2.Location = new System.Drawing.Point(34, 296);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(311, 50);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Return to Login Page if you already \r\nhave an account";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.AliceBlue;
+            this.label1.Location = new System.Drawing.Point(102, 214);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 65);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Sign In";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.AliceBlue;
+            this.label3.Location = new System.Drawing.Point(457, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(437, 65);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Register in to App";
+            // 
+            // cuiButton2
+            // 
+            this.cuiButton2.CheckButton = false;
+            this.cuiButton2.Checked = false;
+            this.cuiButton2.CheckedBackground = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(106)))), ((int)(((byte)(0)))));
+            this.cuiButton2.CheckedForeColor = System.Drawing.Color.White;
+            this.cuiButton2.CheckedImageTint = System.Drawing.Color.White;
+            this.cuiButton2.CheckedOutline = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(106)))), ((int)(((byte)(0)))));
+            this.cuiButton2.Content = "Sign In";
+            this.cuiButton2.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.cuiButton2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cuiButton2.ForeColor = System.Drawing.Color.Black;
+            this.cuiButton2.HoverBackground = System.Drawing.Color.LightGray;
+            this.cuiButton2.HoveredImageTint = System.Drawing.Color.White;
+            this.cuiButton2.HoverForeColor = System.Drawing.Color.White;
+            this.cuiButton2.HoverOutline = System.Drawing.Color.Empty;
+            this.cuiButton2.Image = null;
+            this.cuiButton2.ImageAutoCenter = true;
+            this.cuiButton2.ImageExpand = new System.Drawing.Point(0, 0);
+            this.cuiButton2.ImageOffset = new System.Drawing.Point(0, 0);
+            this.cuiButton2.Location = new System.Drawing.Point(598, 439);
+            this.cuiButton2.Name = "cuiButton2";
+            this.cuiButton2.NormalBackground = System.Drawing.Color.DarkGray;
+            this.cuiButton2.NormalForeColor = System.Drawing.Color.Black;
+            this.cuiButton2.NormalImageTint = System.Drawing.Color.White;
+            this.cuiButton2.NormalOutline = System.Drawing.Color.Empty;
+            this.cuiButton2.OutlineThickness = 1.6F;
+            this.cuiButton2.PressedBackground = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(106)))), ((int)(((byte)(0)))));
+            this.cuiButton2.PressedForeColor = System.Drawing.Color.White;
+            this.cuiButton2.PressedImageTint = System.Drawing.Color.White;
+            this.cuiButton2.PressedOutline = System.Drawing.Color.Empty;
+            this.cuiButton2.Rounding = new System.Windows.Forms.Padding(8);
+            this.cuiButton2.Size = new System.Drawing.Size(153, 45);
+            this.cuiButton2.TabIndex = 11;
+            this.cuiButton2.TextOffset = new System.Drawing.Point(0, 0);
+            // 
+            // cuiFormAnimator1
+            // 
+            this.cuiFormAnimator1.AnimateOnStart = true;
+            this.cuiFormAnimator1.Duration = 400;
+            this.cuiFormAnimator1.EasingType = CuoreUI.Drawing.EasingTypes.QuadOut;
+            this.cuiFormAnimator1.StartOpacity = 0D;
+            this.cuiFormAnimator1.TargetForm = null;
+            this.cuiFormAnimator1.TargetOpacity = 1D;
+            // 
+            // LogInForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(984, 601);
+            this.Controls.Add(this.cuiTextBox22);
+            this.Controls.Add(this.cuiTextBox21);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cuiButton2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "LogInForm";
+            this.Text = "LogInForm";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CuoreUI.Controls.cuiTextBox2 cuiTextBox22;
+        private CuoreUI.Controls.cuiTextBox2 cuiTextBox21;
+        private System.Windows.Forms.Panel panel1;
+        private CuoreUI.Controls.cuiButton cuiButton1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private CuoreUI.Controls.cuiButton cuiButton2;
+        private CuoreUI.Components.cuiFormAnimator cuiFormAnimator1;
+    }
+}
